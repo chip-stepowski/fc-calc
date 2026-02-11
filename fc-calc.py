ed8aff737a1d0ab6f93ec79c3c07b6b42aa98d86
Christopher Stepowski <christopher.stepowski@hashicorp.com>
2026-02-11 14:19:53 -0500

    Added infinite game loop with option to quit.

--- fc-calc.py
+++ fc-calc.py
@@ -1,19 +1,26 @@
-while True:
-    # get user input
-    user_choice = input("Enter '1' for Fahrenheit to Celsius, or '2' for Celsius to Fahrenheit: ")
+import sys
 
-    # check if user input is in this list
-    if user_choice not in ['1', '2']:
-        print(f'Sorry, choice not valid. Please enter 1 or 2.')
+playing = True
+while playing:
+
+    while True:
+        # get user input
+        user_choice = input("Enter '1' for Fahrenheit to Celsius, '2' for Celsius to Fahrenheit, or 'Q' for Quit: ")
+
+        # check if user input is in this list
+        if user_choice not in ['1', '2', 'Q']:
+            print(f'Sorry, choice not valid. Please enter 1 or 2.')
+        elif user_choice == 'Q':
+            sys.exit()
+        else:
+            break
+
+    # calculate based on choice
+    if user_choice == '1':
+        enter_fahrenheit = float(input("Enter a number in Fahrenheit: "))
+        calc_celsisus = (enter_fahrenheit - 32) // (9/5)
+        print(f'{enter_fahrenheit} Fahrenheit is equal to {calc_celsisus} Celsius!')
     else:
-        break
-
-# calculate based on choice
-if user_choice == '1':
-    enter_fahrenheit = float(input("Enter a number in Fahrenheit: "))
-    calc_celsisus = (enter_fahrenheit - 32) // (9/5)
-    print(f'{enter_fahrenheit} Fahrenheit is equal to {calc_celsisus} Celsius!')
-else:
-    enter_celsisus = float(input("Enter a number in Celsius: "))
-    calc_fahrenheit = (enter_celsisus * 9/5) + 32
-    print(f'{enter_celsisus} Celsius is equal to {calc_fahrenheit} Fahrenheit!')
+        enter_celsisus = float(input("Enter a number in Celsius: "))
+        calc_fahrenheit = (enter_celsisus * 9/5) + 32
+        print(f'{enter_celsisus} Celsius is equal to {calc_fahrenheit} Fahrenheit!')
