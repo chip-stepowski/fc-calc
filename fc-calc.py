0dc68103b8a8b6813ee9cf2fbf2fe4678e7ff5f5
Christopher Stepowski <christopher.stepowski@hashicorp.com>
2026-02-11 14:06:52 -0500

    Changed int to float for input.

--- fc-calc.py
+++ fc-calc.py
@@ -1,20 +1,19 @@
 while True:
     # get user input
-    user_choice = input("Enter '1' for Fahrenheit to Celsius, or '2' for Celsius to Fahrenheit: > ")
+    user_choice = input("Enter '1' for Fahrenheit to Celsius, or '2' for Celsius to Fahrenheit: ")
 
-    # check user input is only or 2
+    # check if user input is in this list
     if user_choice not in ['1', '2']:
         print(f'Sorry, choice not valid. Please enter 1 or 2.')
     else:
         break
-        
 
 # calculate based on choice
 if user_choice == '1':
-    enter_fahrenheit = int(input("Enter a number in Fahrenheit: > "))
+    enter_fahrenheit = float(input("Enter a number in Fahrenheit: "))
     calc_celsisus = (enter_fahrenheit - 32) // (9/5)
     print(f'{enter_fahrenheit} Fahrenheit is equal to {calc_celsisus} Celsius!')
 else:
-    enter_celsisus = int(input("Enter a number in Celsius: > "))
+    enter_celsisus = float(input("Enter a number in Celsius: "))
     calc_fahrenheit = (enter_celsisus * 9/5) + 32
     print(f'{enter_celsisus} Celsius is equal to {calc_fahrenheit} Fahrenheit!')
